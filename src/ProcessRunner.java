a51dc75025f3a991d581f3012df8ea847ce58606
fireduck64 <fireduck@gmail.com>
2018-08-02 13:31:43 -0700

    process runner

---
+++ src/ProcessRunner.java
@@ -0,0 +1,69 @@
+package duckutil;
+
+import java.util.Collection;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Runs a process and keeps the standard output and standard error as a string.
+ * Process is done when constructor returns. So good for quick things.
+ */
+public class ProcessRunner
+{
+  StringBuilder err_out;
+  StringBuilder std_out;
+  int exit_val;
+
+  public ProcessRunner(Collection<String> cmd)
+    throws Exception
+  {
+    Process proc = Runtime.getRuntime().exec((String[])cmd.toArray());
+
+    err_out = new StringBuilder();
+    std_out = new StringBuilder();
+
+    PipeThread pt_std = new PipeThread(std_out, proc.getInputStream());
+    pt_std.start();
+
+    PipeThread pt_err = new PipeThread(err_out, proc.getErrorStream());
+    pt_err.start();
+
+    exit_val = proc.waitFor();
+    pt_std.join();
+    pt_err.join();
+
+  }
+
+  public int getReturn(){return exit_val;}
+
+  public String getError(){return err_out.toString();}
+  public String getOutput(){return std_out.toString();}
+
+  public class PipeThread extends Thread
+  {
+    private StringBuilder sb;
+    private InputStream in;
+
+    public PipeThread(StringBuilder sb, InputStream in)
+    {
+      this.sb = sb;
+      this.in = in;
+      
+    }
+
+    public void run()
+    {
+      Scanner scan = new Scanner(in);
+      while(scan.hasNextLine())
+      {
+        String line = scan.nextLine();
+        sb.append(line);
+        sb.append('\n');
+      }
+      scan.close();
+    }
+
+  }
+
+
+}
