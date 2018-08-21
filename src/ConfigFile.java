e8aaf7283a21cb683bb7eb43c18b955accb2d78e
fireduck64 <fireduck@gmail.com>
2018-08-20 21:39:16 -0700

    Config override

--- src/ConfigFile.java
+++ src/ConfigFile.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class ConfigFile extends Config
 {
     private Properties props;
+    private String env_override_prefix;
 
     public ConfigFile(String file_name)
         throws java.io.IOException
@@ -17,11 +18,24 @@ public class ConfigFile extends Config
         props = new Properties();
 
         props.load(new FileInputStream(file_name));
+
+        if (get("env_override_prefix") != null)
+        {
+          env_override_prefix = get("env_override_prefix");
+        }
     }
 
     @Override
     public String get(String key)
     {
-        return props.getProperty(key);
+      if (env_override_prefix != null)
+      {
+        String k = env_override_prefix + key;
+        if (System.getenv().containsKey(k))
+        {
+          return System.getenv().get(k);
+        }
+      }
+      return props.getProperty(key);
     }
 }
